087dbd69bb377dc501968ea9524809c68a2b518b
Maxim Zmeyok <maxim_zmeyok@mail.ru>
2021-11-05 13:11:36 +0300

    feat: solve task 02-write-file

---
+++ 02-write-file/index.js
@@ -0,0 +1,32 @@
+const { stdin, stdout } = process;
+const path = require('path');
+const fs = require('fs');
+
+fs.writeFile(
+    path.join(__dirname, '02-write-file.txt'),
+    '',
+    (err) => {
+        if (err) throw err;
+    }
+);
+
+//console.log('Введите ваш текст: ');
+stdout.write('Введите ваш текст: ');
+
+stdin.on('data', data => {
+    let text = data.toString();
+    if (text == 'exit\r\n') {
+        process.exit();
+    } else {
+        fs.appendFile(
+            path.join(__dirname, '02-write-file.txt'),
+            `${data}`,
+            err => {
+                if (err) throw err;
+            }
+        );
+    }
+});
+
+process.on('exit', () => stdout.write('Удачи в изучении Node.js!'));
+process.on('SIGINT', () => process.exit());
