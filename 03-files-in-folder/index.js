327e6e235ef00479e9f69440990637085f1f98f5
Maxim Zmeyok <maxim_zmeyok@mail.ru>
2021-11-05 23:06:32 +0300

    feat: solve task 03-files-in-folder

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,17 @@
+const path = require('path');
+const fs = require('fs');
+const way = path.join(__dirname, 'secret-folder');
+
+fs.readdir(way, { withFileTypes: true }, (err, files) => {
+    files.forEach(file => {
+        if (!file.isDirectory()) {
+            let name = file.name.split('.')[0];
+            let ext = path.extname(`${file.name}`).split('.')[1];
+            let wayToFile = path.join(way, file.name);
+
+            fs.stat(wayToFile, (err, stats) => {
+                console.log(`${name} - ${ext} - ${stats.size} Bytes`);
+            });
+        };
+    });
+});
