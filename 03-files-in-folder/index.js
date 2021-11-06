3d292d079400d5669de3c4fc07da40230bcebbad
Maxim Zmeyok <maxim_zmeyok@mail.ru>
2021-11-06 17:58:24 +0300

    feat: add solve task 04-copy-directory and refactor code in task 03-files-in-folder

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -5,12 +5,15 @@ const way = path.join(__dirname, 'secret-folder');
 fs.readdir(way, { withFileTypes: true }, (err, files) => {
     files.forEach(file => {
         if (!file.isDirectory()) {
-            let name = file.name.split('.')[0];
+            /* let name = file.name.split('.')[0];
             let ext = path.extname(`${file.name}`).split('.')[1];
             let wayToFile = path.join(way, file.name);
 
             fs.stat(wayToFile, (err, stats) => {
                 console.log(`${name} - ${ext} - ${stats.size} Bytes`);
+            }); */
+            fs.stat(path.join(way, file.name), (err, stats) => {
+                console.log(`${file.name.split('.')[0]} - ${file.name.split('.')[1]} - ${stats.size} Bytes`);
             });
         };
     });
--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,10 @@
+const path = require('path');
+const fs = require('fs');
 
+fs.mkdir(path.join(__dirname, 'files-copy'), { recursive: true }, (err) => {});
+
+fs.readdir(path.join(__dirname, 'files'), { withFileTypes: true }, (err, files) => {
+    files.forEach(file => {
+        fs.copyFile(path.join(__dirname, 'files', file.name), path.join(__dirname, 'files-copy', file.name), (err) => {})
+    });
+});
