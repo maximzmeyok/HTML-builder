9526c3fe18a2ac83ecfebf87a13ca7f2d269660b
Maxim Zmeyok <maxim_zmeyok@mail.ru>
2021-11-07 09:08:37 +0300

    fix: remove folder before copy files

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -3,6 +3,11 @@ const fs = require('fs');
 
 fs.mkdir(path.join(__dirname, 'files-copy'), { recursive: true }, (err) => {});
 
+fs.readdir(path.join(__dirname, 'files-copy'), { withFileTypes: true }, (err, files) => {
+    files.forEach(file => {
+        fs.unlink(path.join(__dirname, 'files-copy', file.name), (err) => {});    });
+});
+
 fs.readdir(path.join(__dirname, 'files'), { withFileTypes: true }, (err, files) => {
     files.forEach(file => {
         fs.copyFile(path.join(__dirname, 'files', file.name), path.join(__dirname, 'files-copy', file.name), (err) => {})
